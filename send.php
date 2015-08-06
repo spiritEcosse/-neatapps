bcf382ee984c78fdad2b2379a3ef1a96f443a0a1
StKotok <stkotok@gmail.com>
2015-08-06 15:11:30 +0300

    form sender: correct message to user

--- send.php
+++ send.php
@@ -10,8 +10,8 @@
 
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
         mail($to, $subject, $message, $headers);
-        echo "Your email was sent!";
+        echo "Email was sent!";
     } else {
-        echo "Invalid Email, please provide an correct email.";
+        echo "Invalid Email.";
     }
 ?>
