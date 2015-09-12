4125e98e1437854f3ae6cd43a8a62ea122ce278c
igor <shevchenkcoigor@gmail.com>
2015-09-12 16:25:22 +0300

    name in form not require, delete min length, fix style in footer, add width to select lang

--- neatapps/forms.py
+++ neatapps/forms.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Feedback(Bootstrap3Form):
-    name = forms.CharField(max_length=30, min_length=3, label=_('Name'), required=True)
-    email = forms.EmailField(required=True, label=_('Email'))
+    name = forms.CharField(max_length=30, label=_('Name'), required=False)
+    email = forms.EmailField(required=True, label=_('Email'), error_messages={'required': _('Please enter your email.')})
     comment = forms.CharField(label=_('Message'), required=True, widget=forms.Textarea(), min_length=1, max_length=3000,
-                              error_messages={'required': _('Enter message.')})
+                              error_messages={'required': _('Please enter your message.')})
